0ed95d1b7a3e6fe6ef300dc4074f3dbf5f8f7e6f
KaelArrstar <artem.vasnev@me.com>
2014-01-10 14:49:35 +0400

    Initial Commit

---
+++ ClipboardMini/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClipboardMini
+//
+//  Created by Artem on 10/01/14.
+//  Copyright (c) 2014 Artem. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ClipboardMini/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ClipboardMini
+//
+//  Created by Artem on 10/01/14.
+//  Copyright (c) 2014 Artem. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
